e206f3b5bd3e225c33d596e18aa9aede199a6ce8
Carina <mllecarinach@gmail.com>
2016-04-11 14:54:42 +0200

    update carina

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
-$(document).ready(function(){
-  $('header li').click(function(){
-   $(this).toggleClass('activeMenu');
-});
+$(document).ready(function () {
+  
+  //Page active menu
+  $('header li').click(function () {
+    $(this).toggleClass('activeMenu');
+  });
+
+
 });
