dc6f62f4311d8a820639891c8e7c2cc16fdf4f57
carinach <mllecarinach@gmail.com>
2016-04-12 17:40:36 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,65 @@
 $(document).ready(function () {
+	
+    //Régler la taille de la page
+  $('.page').css('width', parseInt($('.page').css('width')));
   
-  //Page active menu
-  $('header li').click(function () {
-    $(this).toggleClass('activeMenu');
+  //Fonction pour rétracter le menu gauche
+  $('#hamburger').on('click', function(){
+    $('#left').toggle( "slide" );
+    $('#left').toggleClass('reduced');
+    if($('#left').hasClass('reduced')){
+      console.log('test');
+      $('#reducedleft').css('display', 'block');
+    }
   });
+
+  //Fonction pour la navigation des pages
+  $('#menuNav li').click(function () {
+    $('#menuNav li').removeClass('activeMenu');
+    $('.page').css('display', 'none');
+    $(this).addClass('activeMenu');
+    $('#page-' + $(this).attr('id')).css('display', 'block');
+  });
+  
+  // Fonction pour dérouler la playlist
+  $('#list-playlists').hide();
+  $('#playlists').on('click', function () {
+    $('#list-playlists').toggleClass('opened');
+    if ($('#list-playlists').hasClass('opened')) {
+      $('#list-playlists').show();
+    } else {
+      $('#list-playlists').hide();
+    }
+  });
+  
+  //Fonction pour tourner flèche playlist
+  $( "#playlists a" ).on('click',function() {
+    if (  $( '#arrow' ).css( "transform" ) == 'none' ){
+        $('#arrow').css("transform","rotate(180deg)");
+    } else {
+        $('#arrow').css("transform","" );
+    }
+});
+  
+  //Fonction pour faire scroll le texte
+  var resetHTML = $('.scrollthis').html();
+  var intervalID;
+$(".scrollthis").hover(function(){
+    var $this = $(this);
+    intervalID = setInterval(function() {
+       scroll($this);
+    }, 100);
+}, function() {
+    clearInterval(intervalID);
+});
+
+  $(".scrollthis").mouseout(function(){
+    $(this).html(resetHTML);
+  });
+  
+function scroll(ele){
+    ele.text(function(i,val) { return val.substr(1) + val.substr(0,1); });
+}
 
 /************************************************************
 
@@ -71,14 +127,5 @@ var body = $("#body");
 		Pour changer la couleur du body en fonction de l'heure
 
  ************************************************************/
-// Fonction pour dérouler la playlist
-  $('#list-playlists').hide();
-  $('#playlists').on('click', function(){
-    $('#list-playlists').toggleClass('opened');
-    if($('#list-playlists').hasClass('opened')){
-      $('#list-playlists').show();
-    } else{
-      $('#list-playlists').hide();
-    }
-  });
-});
+
+});//Fin document ready
