fec7bd75be807fd96c28535e107d7470c327288d
MargauxFroidefont <margaux.froidefont@gmail.com>
2016-04-13 11:50:04 +0200

    tfkjn

--- scripts/main.js
+++ scripts/main.js
@@ -124,14 +124,10 @@ $(document).ready(function () {
    ************************************************************/
   
   //Régler la taille de la page
-<<<<<<< HEAD
-  $('.page').css('width', parseInt($('.page').css('width'))-370);
-=======
-  $('.page').css('width', $(window).width() - 370);
-  $(window).resize(function(){
-    $('.page').css('width', $(window).width() - 370);
-  });
->>>>>>> carina
+$('.page').css('width', $(window).width() - 370);
+ $(window).resize(function(){
+   $('.page').css('width', $(window).width() - 370);
+ });
   
   //Fonction pour rétracter le menu gauche
   $('#hamburger').on('click', function(){
