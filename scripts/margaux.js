730fae3ed8e26fc3d0fc8e8b808a4adbf6888a5a
Bérénice David <berenice.david@hotmail.fr>
2016-04-15 00:36:45 +0200

    update margaux.js

--- scripts/margaux.js
+++ scripts/margaux.js
@@ -1,26 +1,17 @@
 $(document).ready(function () {
+
 	function updateAllPlayer() {
 		player.pause();
 		player.currentTime = 0;
 		player.setAttribute("src", music[id_music].url);
 		albumCover.setAttribute("src", music[id_music].albumCover);
 
-  $(".music a").hide();
-  $(".music").hover(
-    function () {
-      $(this).find("a").fadeIn();
-    },
-    function () {
-      $(this).find("a").fadeOut();
-    }
-  );
+		artistPlayer.innerHTML = music[id_music].artist;
+		titlePlayer.innerHTML = music[id_music].title;
+		albumPlayer.innerHTML = music[id_music].albumName;
+		player.play();
+	}
 
-  $("#ambiances-playlists").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-    $(".ambiance").show();
-  });
 	$(".play").on("click", function (e) {
 		e.preventDefault();
 		if ($(this).hasClass("play")) {
@@ -46,112 +37,129 @@ $(document).ready(function () {
 		}
 	});
 
-  $("#genres-playlists").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-    $(".genre").show();
-  });
-  
-    $("#ambiances-mix").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-    $(".ambiance").show();
-  });
+	$(".music a").hide();
+	$(".music").hover(
+		function () {
+			$(this).find("a").fadeIn();
+		},
+		function () {
+			$(this).find("a").fadeOut();
+		}
+	);
 
-  $("#artistes-mix").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-    $(".artistes").show();
-  });
-  
-  $("#podcast-infos").on("click", function () {
-    $(".podcast").hide();
-    $(".emission").hide();
-    $(".podcast").show();
-  });
-  
-  $("#emission-infos").on("click", function () {
-    $(".podcast").hide();
-    $(".emission").hide();
-    $(".emission").show();
-  });
-  
-  $("#electro").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-    $(".electro").show();
-  });
-  
-  $("#chanson_française").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-    $(".chanson_française").show();
-  });
-  
-  $("#musique_classique").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-    $(".classique").show();
-  });
-  
-    $("#tous").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-    $(".genre").show();
-  });
-      
-    $("#pop").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-    $(".pop").show();
-  });
-      
-    $("#rap").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-    $(".rap").show();
-  });
-      
-    $("#rock").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-    $(".rock").show();
-  });
-      
-    $("#jazz").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-  });
-      
-    $("#reggae").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-  });
-      
-    $("#soul").on("click", function () {
-    $(".artistes").hide();
-    $(".genre").hide();
-    $(".ambiance").hide();
-    $(".soul").show();
-  });
-  
-  $(".select").on("click", function(e){
-    e.preventDefault();
-    var genre = $(this).text();
-    localStorage.setItem("genre_selectionné", genre);
-    var genreSelectionné = localStorage.getItem("genre_selectionné");
-    $("#selected-mix").html(genreSelectionné);
-  });
+	$("#ambiances-playlists").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+		$(".ambiance").show();
+	});
+
+	$("#genres-playlists").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+		$(".genre").show();
+	});
+
+	$("#ambiances-mix").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+		$(".ambiance").show();
+	});
+
+	$("#artistes-mix").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+		$(".artistes").show();
+	});
+
+	$("#podcast-infos").on("click", function () {
+		$(".podcast").hide();
+		$(".emission").hide();
+		$(".podcast").show();
+	});
+
+	$("#emission-infos").on("click", function () {
+		$(".podcast").hide();
+		$(".emission").hide();
+		$(".emission").show();
+	});
+
+	$("#electro").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+		$(".electro").show();
+	});
+
+	$("#chanson_française").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+		$(".chanson_française").show();
+	});
+
+	$("#musique_classique").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+		$(".classique").show();
+	});
+
+	$("#tous").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+		$(".genre").show();
+	});
+
+	$("#pop").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+		$(".pop").show();
+	});
+
+	$("#rap").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+		$(".rap").show();
+	});
+
+	$("#rock").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+		$(".rock").show();
+	});
+
+	$("#jazz").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+	});
+
+	$("#reggae").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+	});
+
+	$("#soul").on("click", function () {
+		$(".artistes").hide();
+		$(".genre").hide();
+		$(".ambiance").hide();
+		$(".soul").show();
+	});
+
+	$(".select").on("click", function(e){
+		e.preventDefault();
+		var genre = $(this).text();
+		localStorage.setItem("genre_selectionné", genre);
+		var genreSelectionné = localStorage.getItem("genre_selectionné");
+		$("#selected-mix").html(genreSelectionné);
+	});
 });
